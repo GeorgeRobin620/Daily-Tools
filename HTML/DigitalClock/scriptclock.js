6c9689f2f9fcf2dfa9b7f6647aebff6fc3ca7d63
George Robin <166676897+GeorgeRobin620@users.noreply.github.com>
2024-10-26 14:42:43 -0700

    Delete HTML directory

--- HTML/DigitalClock/scriptclock.js
+++
@@ -1,8 +0,0 @@
-const clock = document.getElementById("clock");
-console.log(clock);
-setInterval(function(){
-    const currDate = new Date();
-    const t = currDate.toLocaleTimeString();
-    clock.innerHTML = t;
-    console.log(currDate);
-},1000)
